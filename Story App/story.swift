eba5f30ed36cf253ae896542c9e698182052134e
Thomas Wicker <thomas@thomaswicker.com>
2017-02-19 17:17:19 -0700

    Adding in story.swift

---
+++ Story App/story.swift
@@ -0,0 +1,21 @@
+//
+//  story.swift
+//  Story App
+//
+//  Created by Thomas Wicker on 2/19/17.
+//  Copyright © 2017 Thomas Wicker. All rights reserved.
+//
+
+import UIKit
+
+enum Story {
+    case returnTrip
+    case touchDown
+    case homeward
+    case rover
+    case cave
+    case crate
+    case monster
+    case droid
+    case home
+}
